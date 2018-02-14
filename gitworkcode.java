a3748d92e6bec31871ae4b827638bb95c9353847
irakli1990 <iraklikardawa@gmail.com>
2018-02-14 14:57:15 +0100

    program is ended last function is done

--- gitworkcode.java
+++ gitworkcode.java
@@ -3,21 +3,22 @@ public class GitWork{
 
 public int sum(int a, int b){
 
-	return ;
+
+	return a+b;
 
 public int sub(int a, int b){
 
-	return ;
+	return a-b;
 
 }
 public int mul(int a, int b){
 
-	return ;
+	return a*b;
 
 }
 public int div(int a, int b){
 
-	return ;
+	return a/b;
 
 }
 
@@ -25,6 +26,11 @@ public int div(int a, int b){
 
 	public static void main(String[] args) {
 		
+
+int i= sum(5,5);
+int j= sub(10,5);
+int k= div(100/5);
+int d= mul(20*5);
 
 
 
